aeabdc46db0a6f4159cae3b365770225e14807da
stekf <pradeepkase2005@gmail.com>
2024-11-22 12:01:09 +0530

    added hash.java

---
+++ hash.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hash {
+    public static void main(String[] args) {
+        HashMap<Integer,String>map=new HashMap<>();
+        map.put(1, "pradeep");
+        map.put(2, "stefan");
+        map.put(3, "nevi");
+        System.out.println(map);
+    }
+}
